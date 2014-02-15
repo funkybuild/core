ca50c2d9b9bc1beab75f565c7ef70256bec7ae1f
Daniel Brolund <daniel.brolund@agical.com>
2014-02-15 13:55:37 +0100

    Added cujojs when

--- test/root.js
+++ test/root.js
@@ -1,1 +1,14 @@
-console.log("hej");
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+requirejs(['when'],
+function   (when) {
+    when('Hejsan').then(console.log);
+});
+
