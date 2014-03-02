d252552ce2f9775a12d3ae3c9a55addc9d163988
Daniel Brolund <daniel.brolund@agical.com>
2014-03-02 11:08:35 +0100

    Removed debug statements

--- test/funkyplay.js
+++ test/funkyplay.js
@@ -7,12 +7,12 @@ requirejs.config({
 
 requirejs(['q', 'lodash', 'funkybuild', 'util'], 
 function (Q,  _, t, util) {
-    function tsk(mess) {
+    function delayedArgAggregator(mess) {
         var str = mess;
         return function() {
             var args = arguments;
             var rand = Math.floor(Math.random()*4001);            
-            console.log(str, 'got:', args, rand);
+            //console.log(str, 'got:', args, rand);
             var d = Q.defer();
             Q.delay(rand).done(function() {
                 d.resolve({name:str, delay:rand, args:args});
@@ -24,16 +24,13 @@ function (Q,  _, t, util) {
     console.log("Testing funkybuild...");
 
     var build = [ 
-        t.task('t1', tsk("Task 1")),
-        t.task('t2', tsk("Task 2")),
-        t.task('t3', ['t1', 't2'], tsk("Task 3"))
+        t.task('t1', delayedArgAggregator("Task 1")),
+        t.task('t2', delayedArgAggregator("Task 2")),
+        t.task('t3', ['t1', 't2'], delayedArgAggregator("Task 3"))
     ]
 
-    var res = t.run (build, ['t3']);
-    console.log("Res:", res);
-
     _.forEach(
-        res,
+        t.run (build, ['t3']),
         function(p) {
             Q.when(p)
                 .then(
